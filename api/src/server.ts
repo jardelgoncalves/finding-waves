04629ed783b51995ccfbbc5efc2d1543be15ab72
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-27 00:41:35 -0300

    feat: setup express and controllers

---
+++ api/src/server.ts
@@ -0,0 +1,29 @@
+import "./util/module-alias";
+import { Server } from "@overnightjs/core";
+import express, { Application } from "express";
+import { ForecastController } from "./controllers/forecast";
+
+export class SetupServer extends Server {
+  constructor(private port = 3001) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+
+    this.addControllers([forecastController])
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+}
