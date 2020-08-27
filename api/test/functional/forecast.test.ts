4706a06994b6cf1cfd8076cfa4abbd4a56a4db76
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-27 00:43:28 -0300

    refactor: using app global (to see jest setup)

--- api/test/functional/forecast.test.ts
+++ api/test/functional/forecast.test.ts
@@ -1,41 +1,46 @@
-import supertest from 'supertest';
-
 describe('Beach forecast functional test', () => {
   it('should return a forecast with just a few times', async () => {
-    const { body, status } = await supertest(app).get('/forecast');
+    const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
-    expect(body).toBe([{
-      "time": "2020-04-26T00:00:00+00:00",
-      "forecast": [{
-        "lat": -33.792726,
-        "lng": 151.289824,
-        "name": "Manly",
-        "position": "E",
-        "rating": 2,
-        "swellDirection": 64.26,
-        "swellHeight": 0.15,
-        "swellPeriod": 3.89,
+    expect(body).toEqual([
+      {
         "time": "2020-04-26T00:00:00+00:00",
-        "waveDirection": 231.38,
-        "waveHeight": 0.47,
-        "windDirection": 299.45
-      }]
-    }, {
-      "time": "2020-04-26T01:00:00+00:00",
-      "forecast": [{
-        "lat": -33.792726,
-        "lng": 151.289824,
-        "name": "Manly",
-        "position": "E",
-        "rating": 2,
-        "swellDirection": 123.41,
-        "swellHeight": 0.21,
-        "swellPeriod": 3.67,
+        "forecast": [
+          {
+            "lat": -33.792726,
+            "lng": 151.289824,
+            "name": "Manly",
+            "position": "E",
+            "rating": 2,
+            "swellDirection": 64.26,
+            "swellHeight": 0.15,
+            "swellPeriod": 3.89,
+            "time": "2020-04-26T00:00:00+00:00",
+            "waveDirection": 231.38,
+            "waveHeight": 0.47,
+            "windDirection": 299.45
+          }
+        ]
+      },
+      {
         "time": "2020-04-26T01:00:00+00:00",
-        "waveDirection": 232.12,
-        "waveHeight": 0.46,
-        "windDirection": 310.48
-      }] }
+        "forecast": [
+          {
+            "lat": -33.792726,
+            "lng": 151.289824,
+            "name": "Manly",
+            "position": "E",
+            "rating": 2,
+            "swellDirection": 123.41,
+            "swellHeight": 0.21,
+            "swellPeriod": 3.67,
+            "time": "2020-04-26T01:00:00+00:00",
+            "waveDirection": 232.12,
+            "waveHeight": 0.46,
+            "windDirection": 310.48
+          }
+        ]
+      }
     ]);
   })
 })
