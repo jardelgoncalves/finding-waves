2c4017bc4749a35ebe2afa11be4979ceca10d90e
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-30 21:50:20 -0300

    refactor: change displayname

--- api/jest.config.js
+++ api/jest.config.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
-  displayName: 'root-tests',
+  displayName: 'unit',
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   testEnvironment: 'node',
   clearMocks: true,
