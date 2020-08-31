552bdcd9145e5f21b9520b2391555ddc5ec310b5
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-30 21:51:24 -0300

    feat: add test to stormglass integration (failure)

---
+++ api/src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.235454;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
