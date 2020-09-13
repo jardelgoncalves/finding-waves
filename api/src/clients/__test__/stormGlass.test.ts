177c66bfd53853dd20c48bb2deb7e35b36a7c9dd
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-13 16:20:39 -0300

    refactor: inferring the types for Axios (mock)

--- api/src/clients/__test__/stormGlass.test.ts
+++ api/src/clients/__test__/stormGlass.test.ts
@@ -6,15 +6,15 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.235454;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
