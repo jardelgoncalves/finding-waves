9f8212f4262253a453d1492bfb671f3c54a08d06
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-13 18:13:30 -0300

    tests: add test to incomplete data

--- api/src/clients/__test__/stormGlass.test.ts
+++ api/src/clients/__test__/stormGlass.test.ts
@@ -19,4 +19,27 @@ describe('StormGlass client', () => {
 
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726;
+    const lng = 151.235454;
+
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2020-04-26T00:00:00+00:00',
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual([]);
+  });
 });
