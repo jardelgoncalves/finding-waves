d4184e291f26b94dbad5018e82394331b3c2578c
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-21 00:11:33 -0300

    refactor: group forecast by time

--- api/src/services/forecast.ts
+++ api/src/services/forecast.ts
@@ -19,12 +19,16 @@ export interface BeachForecast
   extends Omit<Beach, 'user'>,
     ForecastPointNormalized {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async proccessForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const poinstWithCorrectSources: BeachForecast[] = [];
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -38,6 +42,22 @@ export class Forecast {
       }));
       poinstWithCorrectSources.push(...enrichedBeachData);
     }
-    return poinstWithCorrectSources;
+    return this.mapForecastByTime(poinstWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return forecastByTime;
   }
 }
