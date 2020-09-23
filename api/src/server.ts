eafe43ec6909a1b43d7ccc43e46ee75376ab0ffb
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-23 00:51:11 -0300

    feat: add setup database to init method and close server method

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,15 +2,17 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import express, { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
+import * as database from './database';
 
 export class SetupServer extends Server {
   constructor(private port = 3001) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   private setupExpress(): void {
@@ -21,6 +23,14 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
