52ab37520afd8c96528e3cdd8ff9249d6c9e14fd
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-23 00:51:53 -0300

    refactor: add await on init called

--- api/test/jest-setup.ts
+++ api/test/jest-setup.ts
@@ -1,9 +1,14 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
-beforeAll(() => {
-  const server = new SetupServer();
-  server.init();
+let server: SetupServer;
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
 
   global.testRequest = supertest(server.getApp());
+});
+
+afterAll(async () => {
+  server.close();
 });
