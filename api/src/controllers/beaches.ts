5533118fdd4ad9d01b8b0beac88ee481578e6601
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-25 03:08:19 -0300

    feat: add verification error validation

--- api/src/controllers/beaches.ts
+++ api/src/controllers/beaches.ts
@@ -1,13 +1,22 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async creare(req: Request, res: Response): Promise<void> {
-    const beach = new Beach(req.body);
-    const result = await beach.save();
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
 
-    res.status(201).send(result);
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+        return;
+      }
+      res.status(500).send({ error: 'Internal Server Error' });
+    }
   }
 }
