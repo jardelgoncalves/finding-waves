3072aac2bf623e24144e5abdf579ca5a32739b50
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-25 02:57:51 -0300

    refactor: insert in mongodb with model beach

--- api/src/controllers/beaches.ts
+++ api/src/controllers/beaches.ts
@@ -1,9 +1,13 @@
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async creare(req: Request, res: Response): Promise<void> {
-    res.status(201).send({ ...req.body, _id: 'fake-id' });
+    const beach = new Beach(req.body);
+    const result = await beach.save();
+
+    res.status(201).send(result);
   }
 }
