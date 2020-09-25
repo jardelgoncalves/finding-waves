f4986e02c3e3c35b9ba5b806fa736570cd165daf
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-25 03:08:32 -0300

    tests: add test to error validation

--- api/test/functional/beaches.test.ts
+++ api/test/functional/beaches.test.ts
@@ -13,8 +13,23 @@ describe('Beaches funtional tests', () => {
       };
 
       const response = await global.testRequest.post('/beaches').send(newBeach);
-      expect(response.status).toEqual(201);
+      expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 433 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: '151.289824',
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
     });
   });
 });
