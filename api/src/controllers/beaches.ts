18f9fd3fe66827a364523f0758cc4144bcaf85c9
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-24 22:06:53 -0300

    feat: add simple implementation to green test

---
+++ api/src/controllers/beaches.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@overnightjs/core';
+import { Request, Response } from 'express';
+@Controller('beaches')
+export class BeachesController {
+  @Post('')
+  public async creare(req: Request, res: Response): Promise<void> {
+    res.status(201).send(req.body);
+  }
+}
