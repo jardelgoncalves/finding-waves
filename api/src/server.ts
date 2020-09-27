709857cdd7c3cdc4b7e3c9d7300c9c230b196ddd
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-27 15:02:18 -0300

    feat: add start method

--- api/src/server.ts
+++ api/src/server.ts
@@ -35,6 +35,12 @@ export class SetupServer extends Server {
     await database.close();
   }
 
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port:', this.port);
+    });
+  }
+
   public getApp(): Application {
     return this.app;
   }
