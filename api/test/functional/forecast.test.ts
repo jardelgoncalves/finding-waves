d85952a9007a92f7d334de681764c4b3532acb35
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-26 22:36:28 -0300

    refactor: mock api forecast request

--- api/test/functional/forecast.test.ts
+++ api/test/functional/forecast.test.ts
@@ -1,46 +1,40 @@
+import { Beach, BeachPosition } from '@src/models/beach';
+import nock from 'nock';
+import stormGlass3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
+
 describe('Beach forecast functional test', () => {
+  beforeAll(async () => {
+    await Beach.deleteMany({});
+    const defaultBeach = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.E,
+    };
+
+    const beach = new Beach(defaultBeach);
+    await beach.save();
+  });
   it('should return a forecast with just a few times', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlass3HoursFixture);
+
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
-    expect(body).toEqual([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-          },
-        ],
-      },
-    ]);
+    expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
 });
