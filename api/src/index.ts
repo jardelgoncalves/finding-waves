9930004f12f36c9e500a2a32d0d622081e6c8be9
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-27 15:02:30 -0300

    feat: add bootstrap service

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import { SetupServer } from './server';
+import config from 'config';
+
+(async () => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
