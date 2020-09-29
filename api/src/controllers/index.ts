2c19a8a6a3546626c29cb21a08a86ba3cccad416
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-29 01:34:57 -0300

    refactor: separate error handler client

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -8,15 +8,22 @@ export abstract class BaseController {
     error: mongoose.Error.ValidationError | Error
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      const duplicatedKinErrors = Object.values(error.errors).filter(
-        (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
-      );
-      let code = 422;
-      if (duplicatedKinErrors.length) code = 409;
-
-      res.status(code).send({ code, error: error.message });
+      const clientError = this.handlerClientError(error);
+      res.status(clientError.code).send(clientError);
       return;
     }
     res.status(500).send({ code: 500, error: 'Something went wrang!' });
+  }
+
+  private handlerClientError(
+    error: mongoose.Error.ValidationError
+  ): { code: number; error: string } {
+    const duplicatedKinErrors = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+    let code = 422;
+    if (duplicatedKinErrors.length) code = 409;
+
+    return { code, error: error.message };
   }
 }
