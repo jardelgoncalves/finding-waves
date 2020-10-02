ccc7475b042f4004f0ea1bb4af366def5e77a484
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-01 21:41:38 -0300

    tests: add test to user authentication

--- api/test/functional/users.test.ts
+++ api/test/functional/users.test.ts
@@ -56,4 +56,23 @@ describe('Users functional tests', () => {
       });
     });
   });
+
+  describe('when authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: '1234',
+      };
+      await new User(newUser).save();
+
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+  });
 });
