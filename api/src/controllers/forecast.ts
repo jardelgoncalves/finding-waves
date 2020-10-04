c22c9da36953224898ee3ba7293c056c9348de53
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-04 01:47:02 -0300

    refactor: add auth middleware and filtering beach by user

--- api/src/controllers/forecast.ts
+++ api/src/controllers/forecast.ts
@@ -1,18 +1,20 @@
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
 
 const forecast = new Forecast();
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
   @Get('')
   public async getForecastForLoggedUser(
-    _: Request,
+    req: Request,
     res: Response
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.proccessForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
