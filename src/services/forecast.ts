d603dfa7d5f56ffac37a756e9b57c46dbc3e6c24
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-19 01:21:30 -0300

    refactor: add log to forecast

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,4 +1,5 @@
 import { ForecastPointNormalized, StormGlass } from '@src/clients/stormGlass';
+import logger from '@src/logger';
 import { Beach } from '@src/models/beach';
 import { InternalError } from '@src/util/error/internal-error';
 
@@ -23,6 +24,7 @@ export class Forecast {
     beaches: Beach[]
   ): Promise<TimeForecast[]> {
     const poinstWithCorrectSources: BeachForecast[] = [];
+    logger.info(`Preparing the forecast for ${beaches.length} beaches`);
     try {
       for (const beach of beaches) {
         const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -32,6 +34,7 @@ export class Forecast {
       }
       return this.mapForecastByTime(poinstWithCorrectSources);
     } catch (error) {
+      logger.error(error);
       throw new ForecastProcessingInternalError(error.message);
     }
   }
