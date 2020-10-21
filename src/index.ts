d782f0c1eaca9be554a3d9b038cfd896beb88850
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-21 00:35:04 -0300

    refactor add listening of signals command

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,32 @@ import { SetupServer } from './server';
 import config from 'config';
 import logger from './logger';
 
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
 (async () => {
   try {
     const server = new SetupServer(config.get('App.port'));
     await server.init();
     server.start();
+
+    const ExitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    ExitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          logger.info('App exited with Success');
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
   } catch (error) {
     logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
   }
 })();
