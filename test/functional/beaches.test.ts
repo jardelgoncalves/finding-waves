34c6dd61df3e0f85cc48abacf5523d771bb0c460
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-23 01:32:07 -0300

    refactor: change test because was created sendErrorResponse method

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -48,7 +48,9 @@ describe('Beaches funtional tests', () => {
 
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
-        error:
+        code: 422,
+        error: 'Unprocessable Entity',
+        message:
           'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
       });
     });
