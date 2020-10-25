f7038e182731b58e217f9d7996c2481ced12bd3a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-25 20:02:50 -0300

    feat(webapp): add input component

---
+++ webapp/src/components/Input/Input.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'app-input',
+  props: {
+    name: { type: String },
+    value: { type: String }
+  }
+}
+</script>
+
+<template>
+  <input class="input" :name="name" :value="value" @change="$emit('change')" />
+</template>
+<style lang="scss" scoped>
+.input {
+  width: 100%;
+  height: 50px;
+  border: none;
+  border-radius: 12px;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  background-color: var(--bg-input);
+  color: var(--color-secondary);
+  font-weight: bold;
+  font-size: 18px;
+  outline: none;
+  ::placeholder {
+    color: var(--color-placeholder);
+    font-weight: normal;
+    font-size: 16px;
+  }
+  :focus {
+    border: none;
+  }
+}
+</style>
