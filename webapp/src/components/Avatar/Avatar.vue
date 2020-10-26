cce4c81648391cbfb0a5d5255786f88f9534de85
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-26 20:59:16 -0300

    feat: criação do componente de Avatar

---
+++ webapp/src/components/Avatar/Avatar.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'app-avatar',
+  props: {
+    text: { type: String, required: true },
+    color: {type: String, required: false, default: () => 'var(--color-secondary)'},
+    bg: { type: String, required: false, default: () => 'var(--color-primary)' }
+  },
+  computed: {
+    textFinally() {
+      return this.text ? this.text.toUpperCase().slice(0,2) : 'SN'
+    },
+    styles() {
+      return {
+        backgroundColor: this.bg,
+        color: this.color
+      }
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="avatar" :style="styles">
+    {{ textFinally }}
+  </div>
+</template>
+
+<style lang="scss">
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
