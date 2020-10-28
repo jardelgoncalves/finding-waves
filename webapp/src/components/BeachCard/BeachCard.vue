37113ea5d1e5adc1d633b9efa430625c7c1e2599
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-28 00:54:39 -0300

    refactor(webapp): add new properties

--- webapp/src/components/BeachCard/BeachCard.vue
+++ webapp/src/components/BeachCard/BeachCard.vue
@@ -1,12 +1,20 @@
 <script>
+import { Arrow, Star } from '../Icon'
 export default {
   name: 'app-beach-card',
+  components: {
+    'icon-arrow': Arrow,
+    'icon-star': Star,
+  },
   props: {
     name: { type: String, required: true },
     rating: { type: Number, required: false, default: () => 1 },
     swell: { type: String, required: false },
     wind: { type: String, required: false },
-    wave:{ type: String, required: false }
+    wave:{ type: String, required: false },
+    swellRotation:{ type: Number, required: false, default: () => 70 },
+    waveRotation:{ type: Number, required: false, default: () => 70},
+    windRotation:{ type: Number, required: false, default: () => 0},
   },
   methods: {
     styleRating(current) {
@@ -24,31 +32,31 @@ export default {
     <div class="beach-card__info">
       <div class="beach-card__info__col">
         <span class="beach-card__info__col__title">Swell</span>
-        <div class="beach-card__info__col__value">1.21m</div>
+        <div class="beach-card__info__col__value">
+          <icon-arrow :rotate="swellRotation" />
+          {{ swell }}
+        </div>
       </div>
       <div class="beach-card__info__col">
         <span class="beach-card__info__col__title">Wave</span>
-        <div class="beach-card__info__col__value">1.21m</div>
+        <div class="beach-card__info__col__value">
+          <icon-arrow :rotate="waveRotation" />
+          {{ wave }}
+        </div>
       </div>
       <div class="beach-card__info__col">
         <span class="beach-card__info__col__title">Wind</span>
-        <div class="beach-card__info__col__value">1.21m</div>
+        <div class="beach-card__info__col__value">
+          <icon-arrow :rotate="windRotation" />
+          {{ wind }}
+        </div>
       </div>
     </div>
     <div class="beach-card__rating">
       <span class="beach-card__rating__title">Rating</span>
       <div class="beach-card__rating__stars">
         <div v-for="i in 5" :key="i" :class="styleRating(i)">
-          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0)">
-            <path d="M8.10314 0.6258L6.06251 5.28049L1.49689 6.02932C0.67814 6.16291 0.350015 7.29846 0.943765 7.94885L4.24689 11.5699L3.46564 16.6852C3.32501 17.6098 4.19064 18.3024 4.91564 17.8699L9.00002 15.4547L13.0844 17.8699C13.8094 18.2988 14.675 17.6098 14.5344 16.6852L13.7531 11.5699L17.0563 7.94885C17.65 7.29846 17.3219 6.16291 16.5031 6.02932L11.9375 5.28049L9.89689 0.6258C9.53127 -0.203888 8.47189 -0.214435 8.10314 0.6258Z" />
-            </g>
-            <defs>
-            <clipPath id="clip0">
-            <rect width="18" height="18" fill="white"/>
-            </clipPath>
-            </defs>
-          </svg>
+          <icon-star />
         </div>
       </div>
     </div>
@@ -76,7 +84,7 @@ export default {
     flex-direction: row;
     align-items: flex-end;
     justify-content: center;
-    margin-bottom: 18px;
+    margin-bottom: 15px;
 
     &__beach {
       font-size: 12px;
@@ -90,6 +98,7 @@ export default {
       font-size: 22px;
       line-height: 30px;
       color: var(--color-primary);
+      font-weight: bold;
     }
   }
 
@@ -112,6 +121,12 @@ export default {
         font-size: 12px;
         line-height: 16px;
         color: var(--color-primary);
+        display: flex;
+        align-items: center;
+
+        svg {
+          margin-right: 4px;
+        }
       }
     }
   }
