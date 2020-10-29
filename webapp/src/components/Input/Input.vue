3c4fdfc951d12f47e193cfc73bc69cdf5ffd35da
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-10-29 02:39:39 -0300

    feat(webapp): add margin propertie and input type

--- webapp/src/components/Input/Input.vue
+++ webapp/src/components/Input/Input.vue
@@ -2,15 +2,54 @@
 export default {
   name: 'app-input',
   props: {
-    name: { type: String },
-    placeholder: { type: String },
-    value: { type: String }
+    name: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: () => 'text'
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    value: {
+      type: String,
+      required: false,
+    },
+    margin: {
+      type: String,
+      required: false,
+      validator: function(value) {
+        return value && value.split(' ').every(item => ['top', 'bottom', 'left', 'right'].includes(item))
+      }
+    },
+    marginSize: {
+      type: Number,
+      required: false,
+      default: () => 16
+    }
+  },
+  computed: {
+    styles() {
+      const margins = this.margin ? this.margin.split(' ') : []
+      return {
+       ...margins.reduce((acc, orientation) => {
+          const name = `margin-${orientation}`;
+          return {...acc, [name]: `${this.marginSize}px`}
+        }, {})
+      }
+    },
   }
 }
 </script>
 
 <template>
   <input
+    :style="styles"
+    :type="type"
     class="input"
     :name="name"
     :value="value"
@@ -32,9 +71,9 @@ export default {
   font-weight: bold;
   font-size: 18px;
   outline: none;
-  ::placeholder {
+  &::placeholder {
     color: var(--color-placeholder);
-    font-weight: normal;
+    font-weight: 400;
     font-size: 16px;
   }
   :focus {
