1af8dbf1459ab5af0692b6c394b4460e703cea91
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-11-28 01:02:24 -0300

    refactor: change library

--- webapp/src/components/Maps/Maps.vue
+++ webapp/src/components/Maps/Maps.vue
@@ -1,47 +1,41 @@
 <script>
-import GoogleMapsApiLoader from 'google-maps-api-loader'
 
 export default {
   name: 'app-maps',
   data() {
     return {
-      google: null,
-      map: null
+      center: { lat: 45.508, lng: -73.587 },
+      markers: []
     }
   },
   props: {
     mapConfig: Object,
     apiKey: String,
   },
-  async mounted() {
-    const googleMapApi = await GoogleMapsApiLoader({
-      apiKey: this.apiKey
-    })
-    this.google = googleMapApi
-    this.initializeMap()
-  },
-
-  methods: {
-    initializeMap() {
-      const mapContainer = this.$refs.googleMap
-      this.map = new this.google.maps.Map(
-        mapContainer, this.mapConfig
-      )
-    }
-  }
-  
 }
 </script>
 
 <template>
   <div class="maps">
-    <div class="google-map" ref="googleMap"></div>
+    <gmap-map
+      :center="center"
+      :zoom="12"
+      style="width:100%;  height: 400px;"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
   </div>
 </template>
 
 <style lang="scss">
-.maps {
+.app-maps {
   width: 100%;
   height: 400px;
+  background-color: red;
 }
 </style>
