dbec0f31c229bf640a8d6fb74225d0972e0dfc23
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-11-28 01:01:54 -0300

    feat: add config google maps

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_APIKEY,
+  }
+});
 
 Vue.config.productionTip = false
 
