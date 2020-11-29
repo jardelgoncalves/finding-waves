f65b36151bbb72f78c5889aa92fee2b629f33448
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-11-29 13:31:26 -0300

    feature: set token in header

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import * as VueGoogleMaps from "vue2-google-maps";
-import router from './router';
+import * as VueGoogleMaps from "vue2-google-maps"
+import Axios from 'axios'
+import router from './router'
+import { TOKEN } from'./utils/constants'
 
 Vue.use(VueGoogleMaps, {
   load: {
@@ -10,6 +12,12 @@ Vue.use(VueGoogleMaps, {
 });
 
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios
+
+const token = localStorage.getItem(TOKEN)
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   router,
