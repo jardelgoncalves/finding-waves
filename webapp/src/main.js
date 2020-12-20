9c9540489ad15fbb80cae64875f471815a2f7807
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-20 00:04:52 -0300

    feat: add store to Vue instance

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 import * as VueGoogleMaps from "vue2-google-maps"
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import { TOKEN } from'./utils/constants'
 
 Vue.use(Vuex)
@@ -19,10 +20,11 @@ Vue.prototype.$http = Axios
 
 const token = localStorage.getItem(TOKEN)
 if (token) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+  Vue.prototype.$http.defaults.headers.common['x-access-token'] = token
 }
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
