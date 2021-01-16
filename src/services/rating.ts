5575ce9ebb99c48441ae8548b50bedb449a538b2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-16 19:29:20 -0300

    feat: add calculate to rating based on swell height

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -1,5 +1,19 @@
 import { Beach, Position } from '@src/models/beach';
 
+const waveHeights = {
+  ankleToKnee: {
+    min: 0.3,
+    max: 1.0,
+  },
+  waistHigh: {
+    min: 1.0,
+    max: 2.0,
+  },
+  headHigh: {
+    min: 2.0,
+    max: 2.5,
+  },
+};
 export class Rating {
   constructor(private beach: Beach) {}
 
@@ -15,6 +29,28 @@ export class Rating {
   public getRatingForSwellPeriod(period: number): number {
     const rating = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 4, 4, 4, 4, 5];
     return rating[period > 14 ? 14 : period];
+  }
+
+  public getRatingForSwellSize(height: number): number {
+    if (
+      height >= waveHeights.ankleToKnee.min &&
+      height < waveHeights.ankleToKnee.max
+    ) {
+      return 2;
+    }
+
+    if (
+      height >= waveHeights.waistHigh.min &&
+      height < waveHeights.headHigh.max
+    ) {
+      return 3;
+    }
+
+    if (height >= waveHeights.headHigh.min) {
+      return 5;
+    }
+
+    return 1;
   }
 
   private isWindsOffShore(
