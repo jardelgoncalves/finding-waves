3b7d7c3faec63bc9ec80c680bbe1aa5bbf9be960
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-16 18:40:19 -0300

    feat: calculate rating based on swell period

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -3,13 +3,18 @@ import { Beach, Position } from '@src/models/beach';
 export class Rating {
   constructor(private beach: Beach) {}
 
-  getRatingBasedOnWindAndWavePosition(
+  public getRatingBasedOnWindAndWavePosition(
     wavePosition: Position,
     windPosition: Position
   ): number {
     if (wavePosition === windPosition) return 1;
     if (this.isWindsOffShore(wavePosition, windPosition)) return 5;
     return 3;
+  }
+
+  public getRatingForSwellPeriod(period: number): number {
+    const rating = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 4, 4, 4, 4, 5];
+    return rating[period > 14 ? 14 : period];
   }
 
   private isWindsOffShore(
