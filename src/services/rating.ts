5dc24635a7073c9a9c7432919c095e061b5cc0b3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-11 23:01:45 -0300

    fix: add trunct to point

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -45,7 +45,9 @@ export class Rating {
 
   public getRatingForSwellPeriod(period: number): number {
     const rating = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 4, 4, 4, 4, 5];
-    return rating[period > 14 ? 14 : period];
+    const periodTrunc = Math.trunc(period);
+
+    return rating[periodTrunc > 14 ? 14 : periodTrunc];
   }
 
   public getRatingForSwellSize(height: number): number {
