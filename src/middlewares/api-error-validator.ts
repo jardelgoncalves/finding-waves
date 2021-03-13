a5f4303565d605414544d582ed2c0005b4832a1d
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-12 23:02:54 -0300

    feat: add middleware to errors handlers

---
+++ src/middlewares/api-error-validator.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from 'express';
+import ApiError from '@src/util/error/api-error';
+
+export interface HTTPError extends Error {
+  status?: number;
+}
+export function apiErrorValidator(
+  error: HTTPError,
+  _: Partial<Request>,
+  res: Response,
+  __: NextFunction
+): void {
+  const errorCode = error.status || 500;
+  res
+    .status(errorCode)
+    .json(ApiError.format({ code: errorCode, message: error.message }));
+}
