38f15831291c37cd318f4a755741f9bb1bf7448a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-13 22:47:20 -0300

    test: check if is number

--- src/util/__test__/time.test.ts
+++ src/util/__test__/time.test.ts
@@ -1,14 +1,9 @@
-import moment from 'moment';
 import { TimeUtil } from '../time';
 
 describe('time.getUnixForFutureDays test', () => {
   it('should return an unix number to 10 days', () => {
-    moment.now = () => {
-      return +new Date(2020, 1, 1);
-    };
-
     const unix = TimeUtil.getUnixTimeForFutureDays(10);
 
-    expect(unix).toEqual(1581390000);
+    expect(unix).toEqual(expect.any(Number));
   });
 });
